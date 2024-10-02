88cc46e73dc7e2c389daed7b204f462dacee4404
Nivedha Rajendran <nivedha.rajen@gmail.com>
2024-10-02 17:09:13 +0200

    Uni Test Included

--- Start/Minutes/MinutesTests/MinutesTests.swift
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -7,30 +7,27 @@
 //
 
 import XCTest
+@testable import Minutes
 
 final class MinutesTests: XCTestCase {
 
+    var entryTest: Entry!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        entryTest = Entry("Title", "Content")
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        entryTest = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+   func testSerialization() throws {
+       let serializedData = FileEntryStore.serialize(entryTest)
+       let entry = FileEntryStore.deserialize(serializedData!)
+       
+       XCTAssertEqual(entry?.title, "Title", "Title does not match")
+       XCTAssertEqual(entry?.content, "Content", "Content does not match")
     }
 
 }
