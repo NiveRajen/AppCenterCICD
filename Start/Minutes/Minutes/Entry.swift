751b2f0ec71633eb9197820318a9da9d56e382fc
Nivedha Rajendran <nivedha.rajen@gmail.com>
2024-10-02 13:23:55 +0200

    Hello Message

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -3,7 +3,7 @@ import Foundation
 class Entry: Codable
 {
     var title:   String = "test"
-    var content: String = ""
+    var content: String = "hello"
     
     var id: String = UUID().uuidString
     
